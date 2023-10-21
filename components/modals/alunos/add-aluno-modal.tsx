96de881809e57a703fccb771ef6a03e0acd91e7e
leafaar <yyyyrrzb@gmail.com>
2023-10-21 13:23:41 -0300

    FEAT> Add aluno form ready

--- components/modals/alunos/add-aluno-modal.tsx
+++ components/modals/alunos/add-aluno-modal.tsx
@@ -1,4 +1,7 @@
-"use client";
+'use client';
+
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 import {
   Dialog,
@@ -7,35 +10,84 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
+} from '@/components/ui/dialog';
 
-import { Button } from "../../ui/button";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { useModal } from "@/hooks/use-modal-store";
-import { Label } from "../../ui/label";
-import { Input } from "../../ui/input";
-import { Separator } from "../../ui/separator";
+import { Button } from '../../ui/button';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/use-modal-store';
+import { Label } from '../../ui/label';
+import { Input } from '../../ui/input';
+import { Separator } from '../../ui/separator';
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from '@/components/ui/select';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Email inválido.' }).min(5, {
+    message: 'O email deve ter pelo menos 5 caracteres',
+  }),
+  nome: z.string().min(5, {
+    message: 'O nome deve ter pelo menos 5 caracteres',
+  }),
+  telefone: z.string().min(5, {
+    message: 'O telefone deve ter pelo menos 5 caracteres',
+  }),
+  cpf: z.string().min(5, {
+    message: 'O cpf deve ter pelo menos 5 caracteres',
+  }),
+  plano: z.enum(['mensal', 'trimestral', 'semestral', 'anual']),
+  unidade: z.enum(['zonasul', 'zonanorte', 'zonaoeste', 'zonaleste']),
+  responsavelNome: z.string().optional(),
+  responsavelTelefone: z.string().optional(),
+  responsavelCpf: z.string().optional(),
+  responsavelEmail: z.string().email().optional(),
+});
 
 export const AddAlunoModal = () => {
   const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
 
-  const isModalOpen = isOpen && type === "addAluno";
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      cpf: '',
+      nome: '',
+      telefone: '',
+      responsavelNome: '',
+      responsavelTelefone: '',
+      responsavelCpf: '',
+      responsavelEmail: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    form.reset();
+  }
+
+  const isModalOpen = isOpen && type === 'addAluno';
 
   const [isLoading, setIsLoading] = useState(false);
-
-  const addStudent = () => {
-    //preparar form para enviar
-    console.log('entrou aqui')
-  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -45,96 +97,219 @@ export const AddAlunoModal = () => {
             Adicionar Aluno
           </DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col md:gap-8 gap-4">
-          <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="nome">Nome</Label>
-              <Input type="text" id="nome" placeholder="Nome" />
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input type="text" id="email" placeholder="Email" />
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="plano">Plano</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue
-                    placeholder="Selecione um plano"
-                    className="text-slate-500"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col md:gap-8 gap-4">
+              <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Email" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
                   />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="mensal">Mensal</SelectItem>
-                  <SelectItem value="trimestral">Trimestral</SelectItem>
-                  <SelectItem value="semestral">Semestral</SelectItem>
-                  <SelectItem value="anual">Anual</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="telefone">Telefone</Label>
-              <Input type="text" id="telefone" placeholder="Telefone" />
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="cpf">CPF</Label>
-              <Input type="text" id="cpf" placeholder="CPF" />
-            </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="unidade">Unidade</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue
-                    placeholder="Selecione uma unidade"
-                    className="text-slate-500"
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="nome"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Nome</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Nome" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
                   />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="mensal">Zona Sul</SelectItem>
-                  <SelectItem value="trimestral">Zona Leste</SelectItem>
-                  <SelectItem value="semestral">Zona Norte</SelectItem>
-                  <SelectItem value="anual">Zona Oeste</SelectItem>
-                </SelectContent>
-              </Select>
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="plano"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Plano</FormLabel>
+                        <FormControl>
+                          <Select
+                            {...field}
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger>
+                              <SelectValue
+                                placeholder="Selecione um plano"
+                                className="text-slate-500"
+                              />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="mensal">Mensal</SelectItem>
+                              <SelectItem value="trimestral">
+                                Trimestral
+                              </SelectItem>
+                              <SelectItem value="semestral">
+                                Semestral
+                              </SelectItem>
+                              <SelectItem value="anual">Anual</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="telefone"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Telefone</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Telefone" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="cpf"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>CPF</FormLabel>
+                        <FormControl>
+                          <Input placeholder="CPF" {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid w-full  items-center gap-1.5">
+                  <FormField
+                    control={form.control}
+                    name="unidade"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Unidade</FormLabel>
+                        <FormControl>
+                          <Select
+                            {...field}
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger>
+                              <SelectValue
+                                placeholder="Selecione uma unidade"
+                                className="text-slate-500"
+                              />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="zonasul">Zona Sul</SelectItem>
+                              <SelectItem value="zonaleste">
+                                Zona Leste
+                              </SelectItem>
+                              <SelectItem value="zonanorte">
+                                Zona Norte
+                              </SelectItem>
+                              <SelectItem value="zonaoeste">
+                                Zona Oeste
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+              <Separator className="md:my-8 my-2" />
+              <div className="flex flex-col md:gap-6 gap-4">
+                <div className="flex flex-row gap-2 items-center">
+                  <h3>Responsável</h3>
+                  <span className="font-semibold text-slate-900 text-xs uppercase">
+                    OPCIONAL
+                  </span>
+                </div>
+                <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
+                  <div className="grid w-full  items-center gap-1.5">
+                    <FormField
+                      control={form.control}
+                      name="responsavelNome"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Nome</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Nome" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <FormField
+                      control={form.control}
+                      name="responsavelEmail"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Email" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <FormField
+                      control={form.control}
+                      name="responsavelTelefone"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Telefone</FormLabel>
+                          <FormControl>
+                            <Input placeholder="Telefone" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid w-full  items-center gap-1.5">
+                    <FormField
+                      control={form.control}
+                      name="responsavelCpf"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>CPF</FormLabel>
+                          <FormControl>
+                            <Input placeholder="CPF" {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <Separator className="md:my-8 my-2" />
-          <div className="flex flex-col md:gap-6 gap-4">
-            <div className="flex flex-row gap-2 items-center">
-              <h3>Responsável</h3>
-              <span className="font-semibold text-slate-900 text-xs uppercase">
-                OPCIONAL
-              </span>
-            </div>
-            <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
-              <div className="grid w-full  items-center gap-1.5">
-                <Label htmlFor="nome">Nome</Label>
-                <Input type="text" id="nome" placeholder="Nome" />
+            <DialogFooter className="py-6">
+              <div className="flex items-center justify-end w-full gap-2">
+                <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                  Cancelar
+                </Button>
+                <Button disabled={isLoading} type="submit">
+                  Adicionar
+                </Button>
               </div>
-              <div className="grid w-full  items-center gap-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input type="text" id="email" placeholder="Email" />
-              </div>
-
-              <div className="grid w-full  items-center gap-1.5">
-                <Label htmlFor="telefone">Telefone</Label>
-                <Input type="text" id="telefone" placeholder="Telefone" />
-              </div>
-              <div className="grid w-full  items-center gap-1.5">
-                <Label htmlFor="cpf">CPF</Label>
-                <Input type="text" id="cpf" placeholder="CPF" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <DialogFooter className="px-6 py-4">
-          <div className="flex items-center justify-end w-full gap-2">
-            <Button disabled={isLoading} onClick={onClose} variant="ghost">
-              Cancelar
-            </Button>
-            <Button disabled={isLoading} onClick={addStudent}>Adicionar</Button>
-          </div>
-        </DialogFooter>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
