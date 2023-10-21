d08c8412222b08db5aa2103acb92740d3cb40175
leafaar <yyyyrrzb@gmail.com>
2023-10-21 09:16:59 -0300

    FEAT> Lil push

--- components/modals/alunos/pagamentos/pagamento-aluno-modal.tsx
+++ components/modals/alunos/pagamentos/pagamento-aluno-modal.tsx
@@ -36,72 +36,74 @@ export const PagamentoAlunoModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white overflow-hidden md:p-6 p-4 rounded-lg no-scrollbar">
-        <DialogHeader className="justify-center items-center md:py-6 py-4">
-          <DialogTitle className="text-slate-900 font-extrabold md:text-5xl text-2xl">
-            Pagamento Aluno
-          </DialogTitle>
-        </DialogHeader>
-        <div className="flex w-full">
-          <div className="flex flex-col w-full items-center">
-            <h3 className="pb-2 w-full">{data?.alunoNormal?.nome}</h3>
-            <div className="flex gap-1.5 w-full items-center">
-              <PagamentosDataTable
-                columns={pagamentosColumns}
-                data={data?.alunoNormal?.pagamentos || []}
-              />
+      <DialogContent className="bg-white overflow-scroll md:p-6 p-4 rounded-lg no-scrollbar md:w-[80%] w-[95%]">
+        <div className="flex flex-col md:w-full w-[50%] box-border">
+          <DialogHeader className="justify-center items-center md:py-6 py-4 w-full">
+            <DialogTitle className="text-slate-900 font-extrabold md:text-5xl text-2xl">
+              Informações De Pagamentos
+            </DialogTitle>
+          </DialogHeader>
+          <div className="flex w-full">
+            <div className="flex flex-col w-full items-center">
+              <h3 className="pb-2 w-full">{data?.alunoNormal?.nome}</h3>
+              <div className="flex gap-1.5 w-full items-center">
+                <PagamentosDataTable
+                  columns={pagamentosColumns}
+                  data={data?.alunoNormal?.pagamentos || []}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <Separator className="md:my-8 my-2" />
-        <div className="flex flex-col md:gap-6 gap-4">
-          <div className="flex flex-row gap-2 items-center">
-            <h3>Adicionar Pagamento</h3>
-            <span className="font-semibold text-slate-900 text-xs uppercase">
-              OPCIONAL
-            </span>
-          </div>
-          <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="dataPagamento">Data de Pagamento</Label>
-              <Input
-                type="date"
-                id="dataPagamento"
-                placeholder="Data de Pagamento"
-              />
+          <Separator className="md:my-8 my-2" />
+          <div className="flex flex-col md:gap-6 gap-4">
+            <div className="flex flex-row gap-2 items-center">
+              <h3>Adicionar Pagamento</h3>
+              <span className="font-semibold text-slate-900 text-xs uppercase">
+                OPCIONAL
+              </span>
             </div>
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="plano">Plano</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue
-                    placeholder="Selecione um plano"
-                    className="text-slate-500"
-                  />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="mensal">Mensal</SelectItem>
-                  <SelectItem value="trimestral">Trimestral</SelectItem>
-                  <SelectItem value="semestral">Semestral</SelectItem>
-                  <SelectItem value="anual">Anual</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
+            <div className="grid md:grid-cols-[repeat(2,1fr)] grid-cols-[repeat(1,1fr)] grid-rows-[repeat(2,1fr)] gap-x-2 gap-y-2 md:gap-x-8 md:gap-y-4">
+              <div className="grid w-full  items-center gap-1.5">
+                <Label htmlFor="dataPagamento">Data de Pagamento</Label>
+                <Input
+                  type="date"
+                  id="dataPagamento"
+                  placeholder="Data de Pagamento"
+                />
+              </div>
+              <div className="grid w-full  items-center gap-1.5">
+                <Label htmlFor="plano">Plano</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue
+                      placeholder="Selecione um plano"
+                      className="text-slate-500"
+                    />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="mensal">Mensal</SelectItem>
+                    <SelectItem value="trimestral">Trimestral</SelectItem>
+                    <SelectItem value="semestral">Semestral</SelectItem>
+                    <SelectItem value="anual">Anual</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
 
-            <div className="grid w-full  items-center gap-1.5">
-              <Label htmlFor="valor">Valor</Label>
-              <Input type="number" id="valor" placeholder="Valor" />
+              <div className="grid w-full  items-center gap-1.5">
+                <Label htmlFor="valor">Valor</Label>
+                <Input type="number" id="valor" placeholder="Valor" />
+              </div>
             </div>
           </div>
+          <DialogFooter className="px-6 py-4">
+            <div className="flex items-center justify-end w-full gap-2">
+              <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                Cancelar
+              </Button>
+              <Button disabled={isLoading}>Adicionar</Button>
+            </div>
+          </DialogFooter>
         </div>
-        <DialogFooter className="px-6 py-4">
-          <div className="flex items-center justify-end w-full gap-2">
-            <Button disabled={isLoading} onClick={onClose} variant="ghost">
-              Cancelar
-            </Button>
-            <Button disabled={isLoading}>Adicionar</Button>
-          </div>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
