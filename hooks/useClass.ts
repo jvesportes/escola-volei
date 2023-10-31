bfbef681c4b023593dfd11ae6506f1fd69cf4993
leafaar <yyyyrrzb@gmail.com>
2023-10-31 14:43:09 -0300

    FIX> Vercel error with useClass hook.

--- hooks/useClass.ts
+++ hooks/useClass.ts
@@ -1,31 +1,28 @@
-import { api } from "@/services";
-import { turmas } from "@/utils/types";
-import { useEffect, useState } from "react";
+import { api } from '@/services';
+import { turmas } from '@/utils/types';
+import { useEffect, useState } from 'react';
 
 export function useClass() {
   const [data, setData] = useState(turmas);
 
+  // useEffect(() => {
+  //   (async () => {
 
-
-  useEffect(() => {
-    (async () => {
-
-        const result = await api.class.list()
-        if (!result) return;
-        setData(data.map((turma, index) => {
-          return {
-            ...turma,
-            horario: result[index].horario,
-            id: result[index].id,
-            local: result[index].unidade,
-            professor: teacher,
-          }
-        }))
-    })()
-  }, [])
-
+  //       const result = await api.class.list()
+  //       if (!result) return;
+  //       setData(data.map((turma, index) => {
+  //         return {
+  //           ...turma,
+  //           horario: result[index].horario,
+  //           id: result[index].id,
+  //           local: result[index].unidade,
+  //           professor: teacher,
+  //         }
+  //       }))
+  //   })()
+  // }, [])
 
   return {
-    data: turmas
+    data: turmas,
   };
 }
