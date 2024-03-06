8f83f6e95cfbb8532101df73a4d9844b95016fe0
Vinicius Rocha <vinicius.workspace@gmail.com>
2024-03-06 14:05:54 -0300

    feat: add Button component and Skeleton component, update Tooltip styles

--- src/components/shared/ui/dialog.tsx
+++ src/components/shared/ui/dialog.tsx
@@ -7,6 +7,8 @@ import { X } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
+import { Button } from './button';
+
 const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
@@ -46,7 +48,9 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-        <X className="size-4" />
+        <Button size="icon" variant="outline">
+          <X className="size-5" />
+        </Button>
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
