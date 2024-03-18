2ad124474eb1ccf503e999fb429bb9df3a2e92bc
Vinicius Rocha <vinicius.workspace@gmail.com>
2024-03-17 22:41:43 -0300

    refactor: change TabBar component to dynamically render menu items based on user role

--- src/components/layout/TabBar.tsx
+++ src/components/layout/TabBar.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
-import { adminMenuPaths } from '@/constants/menuPaths';
+import { adminMenuPaths, IMenu, teacherMenuPaths } from '@/constants/menuPaths';
 
 import useAuthentication from '@/hooks/useAuthentication';
 
@@ -13,38 +13,42 @@ import { checkIsActive } from '@/utils/checkActive';
 import { Button } from '../shared/ui/button';
 
 export const NavigationTabbar = () => {
-  const router = useRouter();
   const path = usePathname();
   const { isAdmin } = useAuthentication();
+
+  let menuPaths: IMenu[] = teacherMenuPaths;
+
+  if (isAdmin) {
+    menuPaths = adminMenuPaths;
+  }
 
   return (
     <nav
       id="tabbar"
       className="fixed bottom-0 left-0 z-50 inline-flex h-24 w-full items-center justify-center gap-4 bg-zinc-950 px-4 py-2 shadow-[0px_-2px_40px_0px_rgba(0,0,0,0.10)] md:hidden"
     >
-      {isAdmin &&
-        adminMenuPaths.map((item) => (
-          <Link key={item.label} href={item.path} className="w-full">
-            <Button
-              size="icon"
-              className={cn('flex gap-2 size-16 w-full p-3 flex-col items-center h-full')}
+      {menuPaths.map((item) => (
+        <Link key={item.label} href={item.path} className="w-full">
+          <Button
+            size="icon"
+            className={cn('flex gap-2 size-16 w-full p-3 flex-col items-center h-full')}
+          >
+            <item.icon
+              className={cn('size-5', {
+                'text-orange-500': checkIsActive(item.path, path, true),
+              })}
+            />
+            <span
+              className={cn('text-xs', {
+                'text-white bg-zinc-900/50 font-semibold': checkIsActive(item.path, path, true),
+                'text-zinc-400 font-medium': !checkIsActive(item.path, path, true),
+              })}
             >
-              <item.icon
-                className={cn('size-5', {
-                  'text-orange-500': checkIsActive(item.path, path, true),
-                })}
-              />
-              <span
-                className={cn('text-xs', {
-                  'text-white bg-zinc-900/50 font-semibold': checkIsActive(item.path, path, true),
-                  'text-zinc-400 font-medium': !checkIsActive(item.path, path, true),
-                })}
-              >
-                {item.label}
-              </span>
-            </Button>
-          </Link>
-        ))}
+              {item.label}
+            </span>
+          </Button>
+        </Link>
+      ))}
     </nav>
   );
 };
